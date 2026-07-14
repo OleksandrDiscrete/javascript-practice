93aee6abf53627b3d8b6822b5124bae2b438a586
Oleksandr <oleksandr63h@gmail.com>
2026-07-14 16:58:18 +0300

    add-checkboxes-to-random

--- 00-counter/script/randomizer-script.js
+++ 00-counter/script/randomizer-script.js
@@ -1,13 +1,23 @@
 const diceFirst = document.getElementById("diceFirst");
 const diceSecond = document.getElementById("diceSecond");
 const diceThird = document.getElementById("diceThird");
+
 const randomBtn = document.getElementById("randomBtn");
-const array = [diceFirst, diceSecond,diceThird];
+const dices = [diceFirst, diceSecond,diceThird];
+
+const firstDiceCheck = document.getElementById("firstDiceCheck");
+const secondDiceCheck = document.getElementById("secondDiceCheck");
+const thirdDiceCheck = document.getElementById("thirdDiceCheck");
+const checks = [firstDiceCheck, secondDiceCheck, thirdDiceCheck];
 
 randomBtn.onclick = function(){
 
-    for(let i = 0; i < array.length;i++){
-
-            array[i].textContent = Math.ceil(Math.random() * 6);
+    for(let i = 0; i < dices.length;i++){
+            
+        if(checks[i].checked){
+            dices[i].textContent = Math.ceil(Math.random() * 6);
+        }
     }
+
+
 }
