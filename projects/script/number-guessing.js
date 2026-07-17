8669baab1e1adddf0b819ad0356f14deee3c8e8c
Oleksandr <oleksandr63h@gmail.com>
2026-07-17 11:24:33 +0300

    complete-number-guess-game

--- projects/script/number-guessing.js
+++ projects/script/number-guessing.js
@@ -1,7 +1,20 @@
 const numberGuessInput = document.getElementById("numberGuessInput");
 const numberGuessGameBtn = document.getElementById("numberGuessGameBtn");
 
+const maxVal = 4;
+const minVal = 1;
+let answer = Math.floor(Math.random() * (maxVal - minVal + 1)) + minVal; 
+let guess = 0;
 
 numberGuessGameBtn.onclick = function(){
 
+    guess = Number(numberGuessInput.value);
+
+    if(answer === guess){
+        window.alert("Correct!");
+        answer = Math.floor(Math.random() * (maxVal - minVal + 1) + minVal); 
+    }
+    else{
+        window.alert("No!");
+    }
 }
